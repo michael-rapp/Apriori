43e3c8984f28ae0ee31e63658dc5028c5adca18d
Michael Rapp <michael.rapp90@googlemail.com>
2017-05-25 18:08:13 +0200

    Added the class AssociationRule.

---
+++ src/main/java/de/mrapp/apriori/AssociationRule.java
@@ -0,0 +1,159 @@
+/*
+ * Copyright 2016 - 2017 Michael Rapp
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package de.mrapp.apriori;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.Serializable;
+import java.util.Set;
+
+/**
+ * An association rule of the form Y <- X, which consists of a body X and a head Y. Such rules are
+ * given as the result of the Apriori algorithm. Both, the body and the head of an association rule
+ * consist of one or several items. The head and body of a rule must be distinct.
+ *
+ * An association rule specifies, that if the items, which are contained by its body, are frequent,
+ * the items, which are given in its head, can be considered to be frequent as well.
+ *
+ * @author Michael Rapp
+ * @since 1.0.0
+ */
+public class AssociationRule implements Serializable {
+
+    /**
+     * The constant serial version UID.
+     */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * The body of the association rule.
+     */
+    private final Set<Item> body;
+
+    /**
+     * The head of the association rule.
+     */
+    private final Set<Item> head;
+
+    /**
+     * The support of the association rule.
+     */
+    private final double support;
+
+    /**
+     * The confidence of the association rule.
+     */
+    private final double confidence;
+
+    /**
+     * Creates a new association rule.
+     *
+     * @param body       A set, which contains the items, which are contained by the association
+     *                   rule's body, as an instance of the type {@link Set}. The set may neither be
+     *                   null, nor empty
+     * @param head       A set, which contains the items, which are contained by the association
+     *                   rule's head, as an instance of the type {@link Set}. The set may neither be
+     *                   null, nor empty
+     * @param support    The support of the association rule as a {@link Double} value. The support
+     *                   must be at least 0 and at maximum 1
+     * @param confidence The confidence of the association rule as a {@link Double} value. The
+     *                   confidence must be at least 0 and at maximum 1
+     */
+    public AssociationRule(@NotNull final Set<Item> body, @NotNull final Set<Item> head,
+                           final double support,
+                           final double confidence) {
+        // TODO: Throw exceptions
+        this.body = body;
+        this.head = head;
+        this.support = support;
+        this.confidence = confidence;
+    }
+
+    /**
+     * Returns the body of the association rule.
+     *
+     * @return A set, which contains the items, which are contained by the association rule's body,
+     * as an instance of the type {@link Set}. The set may neither be null, nor empty
+     */
+    @NotNull
+    public final Set<Item> getBody() {
+        return body;
+    }
+
+    /**
+     * Returns the head of the association rule.
+     *
+     * @return A set, which contains the items, which are contained by the association rule's head,
+     * as an instance of the type {@link Set}. The set may neither be null nor empty
+     */
+    @NotNull
+    public final Set<Item> getHead() {
+        return head;
+    }
+
+    /**
+     * Returns the support of the association rule. By definition, "support" measures the percentage
+     * of transactions, among all available transactions, for which the rule is true.
+     *
+     * @return The support of the association rule as a {@link Double} value. The support must be at
+     * least 0 and at maximum 1
+     */
+    public final double getSupport() {
+        return support;
+    }
+
+    /**
+     * Returns the confidence of the association rule. By definition, "confidence" measures the
+     * percentage of transactions, among all available transactions, which include all items, which
+     * are contained by the rule's body, as well as by its head.
+     *
+     * @return The confidence of the association rule as a {@link Double} value. The confidence must
+     * be at least 0 and at maximum 1
+     */
+    public final double getConfidence() {
+        return confidence;
+    }
+
+    @Override
+    public final String toString() {
+        return head.toString() + " <- " + body.toString();
+    }
+
+    @Override
+    public final int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + body.hashCode();
+        result = prime * result + head.hashCode();
+        long tempSupport = Double.doubleToLongBits(support);
+        result = prime * result + (int) (tempSupport ^ (tempSupport >>> 32));
+        long tempConfidence = Double.doubleToLongBits(confidence);
+        result = prime * result + (int) (tempConfidence ^ (tempConfidence >>> 32));
+        return result;
+    }
+
+    @Override
+    public final boolean equals(final Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        AssociationRule other = (AssociationRule) obj;
+        return body.equals(other.body) && head.equals(other.head) && support == other.support &&
+                confidence == other.confidence;
+    }
+
+}
---
+++ src/test/java/de/mrapp/apriori/AssociationRuleTest.java
@@ -0,0 +1,123 @@
+/*
+ * Copyright 2016 - 2017 Michael Rapp
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package de.mrapp.apriori;
+
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+
+/**
+ * Tests the functionality of the class {@link AssociationRule}.
+ *
+ * @author Michael Rapp
+ */
+public class AssociationRuleTest {
+
+    /**
+     * Tests, if all class members are set correctly by the constructor.
+     */
+    @Test
+    public final void testConstructor() {
+        Set<Item> body = new HashSet<>();
+        body.add(new ItemImplementation("a"));
+        body.add(new ItemImplementation("b"));
+        Set<Item> head = new HashSet<>();
+        head.add(new ItemImplementation("c"));
+        head.add(new ItemImplementation("d"));
+        double support = 0.5;
+        double confidence = 0.4;
+        AssociationRule associationRule = new AssociationRule(body, head, support, confidence);
+        assertEquals(body, associationRule.getBody());
+        assertEquals(head, associationRule.getHead());
+        assertEquals(support, associationRule.getSupport(), 0);
+        assertEquals(confidence, associationRule.getConfidence(), 0);
+    }
+
+    /**
+     * Tests the functionality of the toString-method.
+     */
+    @Test
+    public final void testToString() {
+        Set<Item> body = new HashSet<>();
+        body.add(new ItemImplementation("a"));
+        body.add(new ItemImplementation("b"));
+        Set<Item> head = new HashSet<>();
+        head.add(new ItemImplementation("c"));
+        head.add(new ItemImplementation("d"));
+        double support = 0.5;
+        double confidence = 0.4;
+        AssociationRule associationRule = new AssociationRule(body, head, support, confidence);
+        assertEquals("[c, d] <- [a, b]", associationRule.toString());
+    }
+
+    /**
+     * Tests the functionality of the hashCode-method.
+     */
+    @Test
+    public final void testHashCode() {
+        Set<Item> body1 = new HashSet<>();
+        body1.add(new ItemImplementation("a"));
+        Set<Item> body2 = new HashSet<>();
+        body2.add(new ItemImplementation("c"));
+        Set<Item> head1 = new HashSet<>();
+        head1.add(new ItemImplementation("b"));
+        Set<Item> head2 = new HashSet<>();
+        head2.add(new ItemImplementation("d"));
+        AssociationRule associationRule1 = new AssociationRule(body1, head1, 0.5, 0.5);
+        AssociationRule associationRule2 = new AssociationRule(body1, head1, 0.5, 0.5);
+        assertEquals(associationRule1.hashCode(), associationRule1.hashCode());
+        assertEquals(associationRule1.hashCode(), associationRule2.hashCode());
+        associationRule2 = new AssociationRule(body2, head1, 0.5, 0.5);
+        assertNotEquals(associationRule1.hashCode(), associationRule2.hashCode());
+        associationRule2 = new AssociationRule(body1, head2, 0.5, 0.5);
+        assertNotEquals(associationRule1.hashCode(), associationRule2.hashCode());
+        associationRule2 = new AssociationRule(body1, head1, 0.6, 0.5);
+        assertNotEquals(associationRule1.hashCode(), associationRule2.hashCode());
+        associationRule2 = new AssociationRule(body1, head1, 0.5, 0.6);
+        assertNotEquals(associationRule1.hashCode(), associationRule2.hashCode());
+    }
+
+    /**
+     * Tests the functionality of the equals-method.
+     */
+    @Test
+    public final void testEquals() {
+        Set<Item> body1 = new HashSet<>();
+        body1.add(new ItemImplementation("a"));
+        Set<Item> body2 = new HashSet<>();
+        body2.add(new ItemImplementation("c"));
+        Set<Item> head1 = new HashSet<>();
+        head1.add(new ItemImplementation("b"));
+        Set<Item> head2 = new HashSet<>();
+        head2.add(new ItemImplementation("d"));
+        AssociationRule associationRule1 = new AssociationRule(body1, head1, 0.5, 0.5);
+        AssociationRule associationRule2 = new AssociationRule(body1, head1, 0.5, 0.5);
+        assertFalse(associationRule1.equals(null));
+        assertFalse(associationRule1.equals(new Object()));
+        assertTrue(associationRule1.equals(associationRule1));
+        assertTrue(associationRule1.equals(associationRule2));
+        associationRule2 = new AssociationRule(body2, head1, 0.5, 0.5);
+        assertFalse(associationRule1.equals(associationRule2));
+        associationRule2 = new AssociationRule(body1, head2, 0.5, 0.5);
+        assertFalse(associationRule1.equals(associationRule2));
+        associationRule2 = new AssociationRule(body1, head1, 0.6, 0.5);
+        assertFalse(associationRule1.equals(associationRule2));
+        associationRule2 = new AssociationRule(body1, head1, 0.5, 0.6);
+        assertFalse(associationRule1.equals(associationRule2));
+    }
+
+}
