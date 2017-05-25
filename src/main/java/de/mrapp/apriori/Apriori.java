0f26a78dae18408f50f86236347f319c1bd395db
Michael Rapp <michael.rapp90@googlemail.com>
2017-05-25 18:22:24 +0200

    Added the class Apriori.

---
+++ src/main/java/de/mrapp/apriori/Apriori.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2017 Michael Rapp
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package de.mrapp.apriori;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * An implementation of the Apriori association rule learning algorithm for mining frequent item
+ * sets. The algorithm processes a set of transactions, of which each one consists of multiple
+ * items, in order to learn association rules.
+ *
+ * @author Michael Rapp
+ * @since 1.0.0
+ */
+public class Apriori {
+
+    /**
+     * Executes the Apriori algorithm on a specific set of transactions in order to learn
+     * association rules, which specify frequent item sets.
+     *
+     * @param iterator An iterator, which allows to iterate the transactions, as an instance of the
+     *                 type {@link Iterable}. The iterable may not be null
+     * @return A set, which contains the association rules, which have been learned by the
+     * algorithm, as an instance of the type {@link Set} or an empty set, if no association rules
+     * have been learned
+     */
+    @NotNull
+    public final Set<AssociationRule> execute(@NotNull final Iterator<Transaction> iterator) {
+        // TODO: Throw exceptions
+        Set<AssociationRule> result = new HashSet<>();
+        // TODO: Implement
+        return result;
+    }
+
+}
