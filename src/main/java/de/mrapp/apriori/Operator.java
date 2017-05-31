0dd1f40394bcc9fc110116eaf2e2b1cd47f76f91
Michael Rapp <michael.rapp90@googlemail.com>
2017-05-31 02:29:36 +0200

    Added the interface Operator.

---
+++ src/main/java/de/mrapp/apriori/Operator.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2017 Michael Rapp
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package de.mrapp.apriori;
+
+/**
+ * Defines the interface, a class, which allows to average the heuristic values, which are
+ * calculated by multiple metrics, must implement.
+ *
+ * @author Michael Rapp
+ * @since 1.0.0
+ */
+public interface Operator {
+
+    /**
+     * Averages the heuristic values, which are calculated by all metrics, which have been added to
+     * the operator.
+     *
+     * @return The averaged heuristic value, which has been calculated, as a {@link Double} value
+     */
+    double average();
+
+}
