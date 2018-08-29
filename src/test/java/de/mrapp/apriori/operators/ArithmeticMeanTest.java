0ef24e8ccbf9e9e949c3129a82b5cf930af46dc5
michael-rapp <michael.rapp@googlemail.com>
2018-08-29 12:18:07 +0200

    Fix tests.

--- src/test/java/de/mrapp/apriori/operators/ArithmeticMeanTest.java
+++ src/test/java/de/mrapp/apriori/operators/ArithmeticMeanTest.java
@@ -14,7 +14,9 @@
 package de.mrapp.apriori.operators;
 
 import de.mrapp.apriori.AssociationRule;
+import de.mrapp.apriori.ItemSet;
 import de.mrapp.apriori.Metric;
+import de.mrapp.apriori.NamedItem;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -30,17 +32,8 @@ import static org.mockito.Mockito.when;
 public class ArithmeticMeanTest {
 
     /**
-     * Ensures, that an {@link IllegalArgumentException} is thrown by the add-methods, when the
-     * metric, which is passed as an argument, is null.
-     */
-    @Test(expected = IllegalArgumentException.class)
-    public final void testAddThrowsExceptionWhenMetricIsNull() {
-        new ArithmeticMean().add(null);
-    }
-
-    /**
-     * Ensures, that an {@link IllegalArgumentException} is thrown by the add methods, when the
-     * weight, which is passes as an argument, is not greater than 0.
+     * Ensures, that an {@link IllegalArgumentException} is thrown by the add methods, when the weight, which is passes
+     * as an argument, is not greater than 0.
      */
     @Test(expected = IllegalArgumentException.class)
     public final void testAddThrowsExceptionWhenWeightIsNotGreaterThanZero() {
@@ -55,7 +48,8 @@ public class ArithmeticMeanTest {
         Metric metric = mock(Metric.class);
         when(metric.evaluate(any())).thenReturn(3d, 5d);
         ArithmeticMean arithmeticMean = new ArithmeticMean().add(metric).add(metric);
-        assertEquals(4, arithmeticMean.evaluate(mock(AssociationRule.class)), 0);
+        AssociationRule<NamedItem> associationRule = new AssociationRule<>(new ItemSet<>(), new ItemSet<>(), 0.0);
+        assertEquals(4, arithmeticMean.evaluate(associationRule), 0);
     }
 
     /**
@@ -66,25 +60,18 @@ public class ArithmeticMeanTest {
         Metric metric = mock(Metric.class);
         when(metric.evaluate(any())).thenReturn(3d, 5d);
         ArithmeticMean arithmeticMean = new ArithmeticMean().add(metric, 2.0).add(metric, 1.0);
-        assertEquals(11d / 3d, arithmeticMean.evaluate(mock(AssociationRule.class)), 0);
+        AssociationRule<NamedItem> associationRule = new AssociationRule<>(new ItemSet<>(), new ItemSet<>(), 0.0);
+        assertEquals(11d / 3d, arithmeticMean.evaluate(associationRule), 0);
     }
 
     /**
-     * Ensures, that an {@link IllegalArgumentException} is thrown by the evaluate-method, when
-     * passing null as a parameter.
-     */
-    @Test(expected = IllegalArgumentException.class)
-    public final void testEvaluateThrowsExceptionWhenRuleIsNull() {
-        new ArithmeticMean().add(mock(Metric.class)).evaluate(null);
-    }
-
-    /**
-     * Ensures, that an {@link IllegalStateException} is thrown by the evaluate-method, when no
-     * metrics have been added.
+     * Ensures, that an {@link IllegalStateException} is thrown by the evaluate-method, when no metrics have been
+     * added.
      */
     @Test(expected = IllegalStateException.class)
     public final void testEvaluateThrowsExceptionWhenNoMetricsAdded() {
-        new ArithmeticMean().evaluate(mock(AssociationRule.class));
+        AssociationRule<NamedItem> associationRule = new AssociationRule<>(new ItemSet<>(), new ItemSet<>(), 0.0);
+        new ArithmeticMean().evaluate(associationRule);
     }
 
 }
